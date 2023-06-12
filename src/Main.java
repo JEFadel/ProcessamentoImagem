151164c7aee60fd4959cea18a0e6768551746e73
roninchris <christiansentd@gmail.com>
2023-06-12 15:25:17 -0300

    Multi Threads Fix

--- src/Main.java
+++ src/Main.java
@@ -9,80 +9,51 @@ public class Main {
     public static final String SOURCE_FILE = "./resources/many-flowers.jpg";
     public static final String DESTINATION_FILE = "./out/many-flowers.jpg";
 
+    public static final int NUM_THREADS = 4; // Define o número de threads a serem utilizadas
+
     public static void main(String[] args) throws IOException {
         BufferedImage originalImage = ImageIO.read(new File(SOURCE_FILE));
         BufferedImage resultImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_RGB);
 
-        // Medir tempo sem threads
-        long startTime = System.currentTimeMillis();
-        recolorSingleThreaded(originalImage, resultImage);
-        long endTime = System.currentTimeMillis();
-        long executionTime = endTime - startTime;
-        System.out.println("Tempo de execução sem threads: " + executionTime + " ms");
+        recolorMultiThreaded(originalImage, resultImage); // Executa o processamento multi-threaded
 
-        // Medir tempo com uma thread
-        startTime = System.currentTimeMillis();
-        recolorMultiThreaded(originalImage, resultImage, 1);
-        endTime = System.currentTimeMillis();
-        executionTime = endTime - startTime;
-        System.out.println("Tempo de execução com 1 thread: " + executionTime + " ms");
-
-        // Medir tempo com duas threads
-        startTime = System.currentTimeMillis();
-        recolorMultiThreaded(originalImage, resultImage, 2);
-        endTime = System.currentTimeMillis();
-        executionTime = endTime - startTime;
-        System.out.println("Tempo de execução com 2 threads: " + executionTime + " ms");
-
-        // Medir tempo com quatro threads
-        startTime = System.currentTimeMillis();
-        recolorMultiThreaded(originalImage, resultImage, 4);
-        endTime = System.currentTimeMillis();
-        executionTime = endTime - startTime;
-        System.out.println("Tempo de execução com 4 threads: " + executionTime + " ms");
-
-        // Gerar gráfico de número de threads x tempo
-        generateThreadTimeGraph(originalImage, resultImage, 1, 8, 1);
-
-        // Fixar o número de threads em 4 e variar o tamanho da imagem
-        generateResolutionSpeedupGraph(originalImage, resultImage, 4, 1.0, 0.1, 0.1);
+        File outputFile = new File(DESTINATION_FILE);
+        ImageIO.write(resultImage, "jpg", outputFile);
     }
 
-    public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage) {
-        recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
-    }
+    public static void recolorMultiThreaded(BufferedImage originalImage, BufferedImage resultImage) {
+        List<Thread> threads = new ArrayList<>();
 
-    public static void recolorMultiThreaded(BufferedImage originalImage, BufferedImage resultImage, int numThreads) {
-        List<ImageProcessor> threads = new ArrayList<>();
-        int imageWidth = originalImage.getWidth();
-        int imageHeight = originalImage.getHeight();
-        int regionWidth = imageWidth / numThreads;
-        int regionHeight = imageHeight / numThreads;
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight();
 
-        for (int i = 0; i < numThreads; i++) {
-            int startX = i * regionWidth;
-            int endX = startX + regionWidth;
-            if (i == numThreads - 1) {
-                // A última thread processa até o final da imagem para cobrir eventuais divisões com resto
-                endX = imageWidth;
-            }
+        int threadWidth = width / NUM_THREADS; // Divide a largura da imagem entre as threads
 
-            ImageProcessor thread = new ImageProcessor(originalImage, resultImage, startX, 0, endX, imageHeight);
+        for (int i = 0; i < NUM_THREADS; i++) {
+            final int threadIndex = i;
+            Thread thread = new Thread(() -> {
+                int leftCorner = threadIndex * threadWidth;
+                int rightCorner = (threadIndex + 1) * threadWidth;
+
+                // A última thread trata qualquer largura restante
+                if (threadIndex == NUM_THREADS - 1) {
+                    rightCorner = width;
+                }
+
+                recolorImage(originalImage, resultImage, leftCorner, 0, rightCorner - leftCorner, height);
+            });
+
             threads.add(thread);
-        }
-
-        // Inicia as threads
-        for (ImageProcessor thread : threads) {
             thread.start();
         }
 
-        // Aguarda a conclusão das threads
-        try {
-            for (ImageProcessor thread : threads) {
+        // Aguarda a conclusão de todas as threads
+        for (Thread thread : threads) {
+            try {
                 thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 
@@ -120,12 +91,6 @@ public class Main {
         setRGB(resultImage, x, y, newRGB);
     }
 
-
-    public static int clamp(int value, int min, int max) {
-        return Math.max(min, Math.min(max, value));
-    }
-
-
     public static void setRGB(BufferedImage image, int x, int y, int rgb) {
         image.getRaster().setDataElements(x, y, image.getColorModel().getDataElements(rgb, null));
     }
@@ -153,76 +118,5 @@ public class Main {
 
     public static int getBlue(int rgb) {
         return rgb & 0x000000FF;
-    }
-
-    public static void generateThreadTimeGraph(BufferedImage originalImage, BufferedImage resultImage, int startThreads,
-                                               int endThreads, int step) {
-        System.out.println("Número de threads x Tempo de execução:");
-
-        for (int numThreads = startThreads; numThreads <= endThreads; numThreads += step) {
-            long startTime = System.currentTimeMillis();
-            recolorMultiThreaded(originalImage, resultImage, numThreads);
-            long endTime = System.currentTimeMillis();
-            long executionTime = endTime - startTime;
-            System.out.println("Threads: " + numThreads + ", Tempo: " + executionTime + " ms");
-        }
-    }
-
-    public static void generateResolutionSpeedupGraph(BufferedImage originalImage, BufferedImage resultImage,
-                                                      int numThreads, double startResolution, double endResolution,
-                                                      double stepResolution) {
-        System.out.println("Speed-up x Resolução:");
-
-        for (double resolution = startResolution; resolution >= endResolution; resolution -= stepResolution) {
-            int newWidth = (int) (originalImage.getWidth() * resolution);
-            int newHeight = (int) (originalImage.getHeight() * resolution);
-            BufferedImage resizedImage = resizeImage(originalImage, newWidth, newHeight);
-
-            BufferedImage resizedResultImage = new BufferedImage(resizedImage.getWidth(), resizedImage.getHeight(),
-                    BufferedImage.TYPE_INT_RGB);
-
-            long singleThreadStartTime = System.currentTimeMillis();
-            recolorSingleThreaded(resizedImage, resizedResultImage);
-            long singleThreadEndTime = System.currentTimeMillis();
-            long singleThreadExecutionTime = singleThreadEndTime - singleThreadStartTime;
-
-            long multiThreadStartTime = System.currentTimeMillis();
-            recolorMultiThreaded(resizedImage, resizedResultImage, numThreads);
-            long multiThreadEndTime = System.currentTimeMillis();
-            long multiThreadExecutionTime = multiThreadEndTime - multiThreadStartTime;
-
-            double speedup = (double) singleThreadExecutionTime / multiThreadExecutionTime;
-            System.out.println("Resolução: " + resolution + ", Speed-up: " + speedup);
-        }
-    }
-
-    public static BufferedImage resizeImage(BufferedImage originalImage, int newWidth, int newHeight) {
-        BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, originalImage.getType());
-        resizedImage.getGraphics().drawImage(originalImage, 0, 0, newWidth, newHeight, null);
-        return resizedImage;
-    }
-}
-
-class ImageProcessor extends Thread {
-    private BufferedImage originalImage;
-    private BufferedImage resultImage;
-    private int leftCorner;
-    private int topCorner;
-    private int width;
-    private int height;
-
-    public ImageProcessor(BufferedImage originalImage, BufferedImage resultImage, int leftCorner, int topCorner,
-                          int width, int height) {
-        this.originalImage = originalImage;
-        this.resultImage = resultImage;
-        this.leftCorner = leftCorner;
-        this.topCorner = topCorner;
-        this.width = width;
-        this.height = height;
-    }
-
-    @Override
-    public void run() {
-        Main.recolorImage(originalImage, resultImage, leftCorner, topCorner, width, height);
     }
 }
